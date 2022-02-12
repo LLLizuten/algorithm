3f57aba30e56fd8a8e4e4f609c053c63f2a55910
lzy <896002844@qq.com>
2022-02-12 14:06:38 +0800

    second

---
+++ data-structures/src/test/FirstTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * @author lzy
+ * @create 2021-12-13 23:36
+ */
+public class FirstTest {
+    @Test
+    public void test(){
+        System.out.println("hello");
+    }
+}
