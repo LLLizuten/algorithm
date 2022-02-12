e55c4e826d3c51fcd309aac6563a904f471c12b2
lzy <896002844@qq.com>
2022-02-12 14:24:35 +0800

    hot-fix-test

--- data-structures/src/test/FirstTest.java
+++ data-structures/src/test/FirstTest.java
@@ -13,5 +13,6 @@ public class FirstTest {
     public void test(){
         System.out.println("hello");
         System.out.println("hot-fix-1");
+        System.out.println("hot-fix-test");
     }
 }
